8a8f2e03b2a8e9a12d34cfd387b5f23909d64dc3
0x0w1z <103829929+0x0w1z@users.noreply.github.com>
2025-01-16 22:48:53 +0545

    Delete Greenish_Gruvbox directory

--- Greenish_Gruvbox/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    export PATH="/home/linux/.deta/bin:$PATH"
-    export PATH="/home/linux/.local/bin:$PATH"
-end
